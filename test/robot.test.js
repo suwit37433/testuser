172b02fb67001f552e4dea07c62f2190df70162c
suwit37433 <31337960+suwit37433@users.noreply.github.com>
2017-09-16 16:27:12 +0700

    Update robot.test.js

--- test/robot.test.js
+++ test/robot.test.js
@@ -1,7 +1,7 @@
 var should = require('should');
  
 describe('Robot', function(){
-  var robot = require('../lib/robot');
+  var robot = require('../robot');
  
 it('should say Good morning when someone say Hi between 0:00 - 12:00', function(done){
     var dt = new Date(2013, 11, 26, 0, 0, 0);
